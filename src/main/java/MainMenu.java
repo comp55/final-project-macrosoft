24dd38a237a8ef9406a8384c7eb870943b68ef3d
kli35 <kli35@STK-ECS-JR3R243.stk.pacific.edu>
2024-03-27 12:08:32 -0700

    added todo list
    added necessary methods

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -1,4 +1,15 @@
+import javax.swing.*;
 
 public class MainMenu {
 
+	private static final int WINDOW_X = 1024;
+	private static final int WINDOW_Y = 768;
+	
+	public MainMenu() {
+	
+	}
+	
+	public static void main(String[] args) {
+		new MainMenu();
+	}
 }
