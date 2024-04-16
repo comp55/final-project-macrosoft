be08bdf11686692bad2989fd8457864a96c6cde4
kfli35 <k_li32@u.pacific.edu>
2024-04-16 09:46:47 -0700

    Added sounds and background music looping

--- src/main/java/BackgroundMusic.java
+++ src/main/java/BackgroundMusic.java
@@ -9,9 +9,11 @@ public class BackgroundMusic {
     private String filePath;
     private AdvancedPlayer player;
     private Thread musicThread;
+    private boolean loop;
 
-    public BackgroundMusic(String filePath) {
+    public BackgroundMusic(String filePath, boolean loop) {
         this.filePath = filePath;
+        this.loop = loop;
     }
 
     public void play() {
@@ -19,10 +21,12 @@ public class BackgroundMusic {
             @Override
             public void run() {
                 try {
-                    FileInputStream fileInputStream = new FileInputStream(filePath);
-                    BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-                    player = new AdvancedPlayer(bufferedInputStream);
-                    player.play();
+                	do {
+                        FileInputStream fileInputStream = new FileInputStream(filePath);
+                        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+                        player = new AdvancedPlayer(bufferedInputStream);
+                        player.play();
+                	} while (loop);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 } catch (JavaLayerException e) {
@@ -35,6 +39,7 @@ public class BackgroundMusic {
 
     public void stop() {
         if (player != null) {
+        	loop = false;
             player.close();
         }
         if (musicThread != null) {
