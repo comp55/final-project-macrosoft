aa9f620adf3c26baff89f166af3c244d35f28863
Charlie Ostner <c_ostner@u.pacific.edu>
2024-04-25 23:36:07 -0700

    Fixed some music, one of the maps, and started working on deleting old comments

--- src/main/java/Platformer.java
+++ src/main/java/Platformer.java
@@ -1,29 +1,3 @@
-/*
- * Copyright (c) 2010-2022 William Bittle  http://www.dyn4j.org/
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are permitted 
- * provided that the following conditions are met:
- * 
- *   * Redistributions of source code must retain the above copyright notice, this list of conditions 
- *     and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
- *     and the following disclaimer in the documentation and/or other materials provided with the 
- *     distribution.
- *   * Neither the name of dyn4j nor the names of its contributors may be used to endorse or 
- *     promote products derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-//package org.dyn4j.samples;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
@@ -208,15 +182,6 @@ public class Platformer extends SimulationFrame {
 
 		initPlayers(numPlayers);
 		//initUI(numPlayers);
-
-		// Use a number of concepts here to support movement, jumping, and one-way
-		// platforms - this is by no means THE solution to these problems, but just
-		// and example to provide some ideas on how you might
-
-		// One consideration might be to use a sensor body to get less accurate
-		// on-ground detection so that it's not frustrating to the user. dyn4j
-		// will detect them in collision, but small bouncing or other things could
-		// cause it to look/feel wrong
 
 		// SETP 1:
 		// at the beginning of each world step, check if the body is in
@@ -743,6 +708,7 @@ public class Platformer extends SimulationFrame {
 	}
 
 	private void closeToMenu() {
+		bgMusic.stop();
 		MainMenu m = new MainMenu();
 		this.stop();
 		this.dispose();
